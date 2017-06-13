896fb960d1f172dbdc0c85b46ad7ad6df86f65bc
Alexander Simson <alex@simsons.se>
2017-06-13 15:51:17 +0200

    Add some basic tests to the example project.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,6 +6,7 @@
 //
 
 @import XCTest;
+@import Castle;
 
 @interface Tests : XCTestCase
 
@@ -25,9 +26,50 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testConfiguration
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    // Create configuration object
+    CastleConfiguration *configuration = [CastleConfiguration configurationWithPublishableKey:@"pk_SE5aTeotKZpDEn8kurzBYquRZyy21fvZ"];
+    configuration.screenTrackingEnabled = YES;
+    configuration.debugLoggingEnabled = YES;
+    configuration.deviceIDAutoForwardingEnabled = YES;
+    configuration.flushLimit = 10;
+    configuration.baseURLWhiteList = @[ [NSURL URLWithString:@"https://google.com/"] ];
+    
+    // Setup Castle SDK with provided configuration
+    [Castle setupWithConfiguration:configuration];
+    
+    // Check that all the configuration parameters where set correctly
+    XCTAssertEqual(configuration.publishableKey, @"pk_SE5aTeotKZpDEn8kurzBYquRZyy21fvZ");
+    XCTAssertEqual(configuration.screenTrackingEnabled, YES);
+    XCTAssertEqual(configuration.debugLoggingEnabled, YES);
+    XCTAssertEqual(configuration.deviceIDAutoForwardingEnabled, YES);
+    XCTAssertEqual(configuration.flushLimit, 10);
+    XCTAssertEqual(configuration.baseURLWhiteList.count, 1);
+    XCTAssertEqual(configuration.baseURLWhiteList[0].absoluteString, [NSURL URLWithString:@"https://google.com/"].absoluteString);
+}
+
+- (void)testDeviceIdentifier
+{
+    // Check device ID
+    XCTAssertNotNil([Castle deviceIdentifier]);
+}
+
+- (void)testUserIdPersistance
+{
+    // Make sure the user id is persisted correctly after identify
+    [Castle identify:@"thisisatestuser"];
+    
+    // Check that the stored identity is the same as the identity we tracked
+    XCTAssertEqual([Castle userIdentity], @"thisisatestuser");
+}
+
+- (void)testReset
+{
+    [Castle reset];
+    
+    // Check to see if the user identity was cleared on reset
+    XCTAssertNil([Castle userIdentity]);
 }
 
 @end
