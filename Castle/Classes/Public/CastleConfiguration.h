f5fe994742b5cadd955e1883ba249dc64405abe5
Alexander Simson <alex@simsons.se>
2019-01-31 15:05:06 +0100

    Add documentation for public classes.

--- Castle/Classes/Public/CastleConfiguration.h
+++ Castle/Classes/Public/CastleConfiguration.h
@@ -7,19 +7,57 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ This object provides configuration options used to initialize the Castle SDK.
+ Changes to the configuration object won't have any affect on the SDK configuration once configured.
+ */
 @interface CastleConfiguration : NSObject
 
+/**
+ Castle publishable key
+ */
 @property (nonatomic, copy, readonly) NSString * _Nonnull publishableKey;
 
+/**
+ Automatic screen tracking enabled
+ */
 @property (nonatomic, assign, getter=isScreenTrackingEnabled) BOOL screenTrackingEnabled;
+
+/**
+ Debug logging enabled
+ */
 @property (nonatomic, assign, getter=isDebugLoggingEnabled) BOOL debugLoggingEnabled;
+
+/**
+ Device ID auto forwarding enabled
+ */
 @property (nonatomic, assign, getter=isDeviceIDAutoForwardingEnabled) BOOL deviceIDAutoForwardingEnabled;
 
+/**
+ The upper limit for stored events in the event queue
+ */
 @property (nonatomic, assign) NSUInteger maxQueueLimit;
+
+/**
+ The number of events stored before flushing the queue
+ */
 @property (nonatomic, assign) NSUInteger flushLimit;
 
+/**
+ Base url whitelist
+ */
 @property (nonatomic, strong, readwrite) NSArray<NSURL *> * _Nonnull baseURLWhiteList;
 
+
+/**
+ Default configuration with provided publishable key
+
+ @param publishableKey Castle publishable key
+ @return CastleConfiguration instance with default settings
+ @code // Create configuration object
+ CastleConfiguration *configuration = [CastleConfiguration configurationWithPublishableKey:@"pk_373428597387773"];
+ @endcode
+ */
 + (instancetype _Nonnull)configurationWithPublishableKey:(NSString * _Nonnull)publishableKey;
     
 @end
