6ff45ab4d6fcf7fcbe91bc8baa7ef4906111f319
Alexander Simson <alex@simsons.se>
2021-05-18 12:08:00 +0200

    Lint.

--- Castle/Public/Castle.m
+++ Castle/Public/Castle.m
@@ -29,7 +29,7 @@ NSString *const CastleUserIdentifierKey = @"CastleUserIdentifierKey";
 NSString *const CastleSecureSignatureKey = @"CastleSecureSignatureKey";
 NSString *const CastleAppVersionKey = @"CastleAppVersionKey";
 
-NSString *const CastleClientIdHeaderName =  @"X-Castle-Client-Id";
+NSString *const CastleClientIdHeaderName = @"X-Castle-Client-Id";
 
 static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 
@@ -87,26 +87,26 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
     Castle *castle = [Castle sharedInstance];
     castle.client = [CASAPIClient clientWithConfiguration:configuration];
     castle.configuration = configuration;
-
+    
     // Highwind
     castle.highwind = [[Highwind alloc] initWithVersion:Castle.versionString uuid:castle.deviceIdentifier userAgent: CASUserAgent()];
     
     castle.reachability = [CASReachability reachabilityWithHostname:@"google.com"];
     [castle.reachability startNotifier];
-
+    
     // Initialize interceptor
     if(configuration.isDeviceIDAutoForwardingEnabled) {
         [NSURLProtocol registerClass:[CASRequestInterceptor class]];
     }
-
+    
     // Initialize automatic screen events
     if(configuration.isScreenTrackingEnabled) {
         [UIViewController ca_swizzleViewDidAppear];
     }
-
+    
     // Update debug logging setting
     CASEnableDebugLogging([configuration isDebugLoggingEnabled]);
-
+    
     // Track application update/install
     [castle trackApplicationUpdated];
 }
@@ -121,13 +121,13 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     Castle *castle = [Castle sharedInstance];
     CastleConfiguration *configuration = castle.configuration;
-
+    
     // Reset Castle shared instance properties
     castle.client = nil;
     castle.configuration = nil;
     castle.reachability = nil;
     CASEnableDebugLogging(NO);
-
+    
     // Unregister request interceptor
     if(configuration.isDeviceIDAutoForwardingEnabled) {
         [NSURLProtocol unregisterClass:[CASRequestInterceptor class]];
@@ -203,7 +203,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 - (void)setUserId:(NSString *)userId
 {
     _userId = userId;
-
+    
     // Store user identity in user defaults
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:userId forKey:CastleUserIdentifierKey];
@@ -213,7 +213,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 - (void)setUserSignature:(NSString *)userSignature
 {
     _userSignature = userSignature;
-
+    
     // Store user signature in user defaults
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:userSignature forKey:CastleSecureSignatureKey];
@@ -233,7 +233,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"No event name provided. Will cancel track event operation.");
         return;
     }
-
+    
     Castle *castle = [Castle sharedInstance];
     CASEvent *event = [CASEvent eventWithName:eventName properties:properties];
     [castle queueEvent:event];
@@ -250,7 +250,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"No screen name provided. Will cancel track event operation.");
         return;
     }
-
+    
     Castle *castle = [Castle sharedInstance];
     CASScreen *screen = [CASScreen eventWithName:screenName properties:properties];
     [castle queueEvent:screen];
@@ -267,17 +267,17 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"No user id provided. Will cancel identify operation.");
         return;
     }
-
+    
     Castle *castle = [Castle sharedInstance];
     if(![castle secureModeEnabled]) {
         CASLog(@"Identify called without secure mode user signature set. If secure mode is enabled in Castle and identify is called before secure, the identify event will be discarded.");
     }
-
+    
     CASIdentity *identity = [CASIdentity identityWithUserId:userId traits:traits];
     if(identity != nil) {
         [castle setUserId:userId];
         [castle queueEvent:identity];
-
+        
         // Identify call will always flush
         [Castle flush];
     }
@@ -289,7 +289,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"No user signature provided. Will cancel secure operation.");
         return;
     }
-
+    
     Castle *castle = [Castle sharedInstance];
     [castle setUserSignature:userSignature];
 }
@@ -297,49 +297,49 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 + (void)flush
 {
     __block Castle *castle = [Castle sharedInstance];
-
+    
     if(castle.task != nil) {
         CASLog(@"Queue is already being flushed. Won't flush again.");
         return;
     }
-
+    
     NSArray *batch = @[];
     if ([castle.eventQueue count] >= castle.maxBatchSize) {
         batch = [castle.eventQueue subarrayWithRange:NSMakeRange(0, castle.maxBatchSize)];
     } else {
         batch = [NSArray arrayWithArray:castle.eventQueue];
     }
-
+    
     CASLog(@"Flushing %ld of %ld queued events", batch.count, castle.eventQueue.count);
-
+    
     __block CASBatch *batchModel = [CASBatch batchWithEvents:batch];
-
+    
     // Nil batch model object means there's no events to flush
     if(!batchModel) {
         return;
     }
-
+    
     castle.task = [castle.client dataTaskWithPath:@"batch" postData:[batchModel JSONData] completion:^(id responseObject, NSURLResponse *response, NSError *error) {
         if(error != nil) {
             CASLog(@"Flush failed with error: %@", error);
             castle.task = nil;
             return;
         }
-
+        
         // Remove successfully flushed events from queue and persist
         [castle.eventQueue removeObjectsInArray:batchModel.events];
         [castle persistQueue];
-
+        
         castle.task = nil;
-
+        
         CASLog(@"Successfully flushed (%ld) events: %@", batchModel.events.count, [batchModel JSONPayload]);
-
+        
         if ([castle eventQueueExceedsFlushLimit] && castle.eventQueue.count > 0) {
             CASLog(@"Current event queue still exceeds flush limit. Flush again");
             [Castle flush];
         }
     }];
-
+    
     [castle.task resume];
 }
 
@@ -354,17 +354,17 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     Castle *castle = [Castle sharedInstance];
     CASLog(@"(%ld) event in queue. Resetting event queue.", castle.eventQueue.count);
-
+    
     // Cancel any running flush task
     [castle.task cancel];
     castle.task = nil;
-
+    
     // Flush queue
     [Castle flush];
-
+    
     // Reset cached user id
     castle.userId = nil;
-
+    
     // Reset cached signature
     castle.userSignature = nil;
 }
@@ -375,7 +375,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"Provided allowlist URL was nil");
         return NO;
     }
-
+    
     Castle *castle = [Castle sharedInstance];
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self.host = %@ AND self.scheme = %@", url.host, url.scheme];
     return [castle.configuration.baseURLAllowList filteredArrayUsingPredicate:predicate].count > 0;
@@ -400,17 +400,17 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"Can't enqueue nil event");
         return;
     }
-
+    
     // Trim queue before adding element to make sure it never exceeds maxQueueLimit
     [self trimQueue];
-
+    
     // Add event to the queue
     CASLog(@"Queing event: %@", event);
     [self.eventQueue addObject:event];
-
+    
     // Persist queue to disk
     [self persistQueue];
-
+    
     // Flush queue if the number of events exceeds the flush limit
     if(self.eventQueue.count >= self.configuration.flushLimit) {
         // very first event should be fired immediately
@@ -423,12 +423,12 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     // Trim queue to maxQueueLimit - 1. This method is only called when queuing an event
     NSUInteger maxQueueLimit = self.configuration.maxQueueLimit - 1;
-
+    
     // If the queue doesn't exceed maxQueueLimit just return
     if(self.eventQueue.count < maxQueueLimit) {
         return;
     }
-
+    
     // Remove the oldest excess events from the queue
     NSRange trimRange = NSMakeRange(0, self.eventQueue.count - maxQueueLimit);
     CASLog(@"Queue (size %ld) will exceed maxQueueLimit (%ld). Will trim %ld events from queue.", self.eventQueue.count, self.configuration.maxQueueLimit, trimRange.length);
@@ -446,13 +446,13 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *currentVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
     NSString *installedVersion = [defaults objectForKey:CastleAppVersionKey];
-
+    
     if (installedVersion == nil) {
         // This means that the application was just installed.
         CASLog(@"No app version was stored in settings: the application was just installed.");
         CASLog(@"Application life cycle event detected: Will track install event");
         [Castle track:@"Application installed"];
-
+        
         // Flush the event queue when a application installed event is triggered
         [Castle flush];
     } else if (![installedVersion isEqualToString:currentVersion]) {
@@ -460,11 +460,11 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         CASLog(@"App version stored in settings is different from current version string: the application was just updated.");
         CASLog(@"Application life cycle event detected: Will track update event");
         [Castle track:@"Application updated"];
-
+        
         // Flush the event queue when a application updated event is triggered
         [Castle flush];
     }
-
+    
     [defaults setObject:currentVersion forKey:CastleAppVersionKey];
     [defaults synchronize];
 }
@@ -480,7 +480,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     CASLog(@"Application life cycle event detected: Will track application did become active event");
     [Castle track:@"Application Did Become Active"];
-
+    
     // Flush the event queue when a application did become active event is triggered
     [Castle flush];
 }
@@ -489,7 +489,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     CASLog(@"Application life cycle event detected: Will track application did enter background event");
     [Castle track:@"Application Did Enter Background"];
-
+    
     // Flush the event queue when a application did enter background event is triggered
     [Castle flush];
 }
@@ -498,7 +498,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 {
     CASLog(@"Application life cycle event detected: Will track application will terminate event");
     [Castle track:@"Application Will Terminate"];
-
+    
     // Flush the event queue when a application will terminate event is triggered
     [Castle flush];
 }
