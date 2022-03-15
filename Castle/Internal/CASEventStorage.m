ec8fa6a5a089b2b81dd28087da926c7de2e77677
Alexander Simson <alex@simsons.se>
2022-03-15 16:00:26 +0100

    Clear event queue if there is an error when reading.
    Signed-off-by: Alexander Simson <alex@simsons.se>

--- Castle/Internal/CASEventStorage.m
+++ Castle/Internal/CASEventStorage.m
@@ -8,6 +8,9 @@
 #import "CASEventStorage.h"
 
 #import "CASUtils.h"
+#import "CASEvent.h"
+#import "CASScreen.h"
+#import "CASCustom.h"
 
 @implementation CASEventStorage
 
@@ -15,7 +18,27 @@
 
 + (NSArray *)storedQueue
 {
-    NSArray *queue = [NSKeyedUnarchiver unarchiveObjectWithFile:self.storagePath];
+    NSArray *queue = @[];
+    if (@available(iOS 14.0, *)) {
+        NSError *error = nil;
+        NSData *data = [NSData dataWithContentsOfFile:self.storagePath];
+        NSSet *classes = [NSSet setWithArray:@[CASEvent.self, CASScreen.self, CASCustom.self]];
+        queue = [NSKeyedUnarchiver unarchivedArrayOfObjectsOfClasses:classes fromData:data error:&error];
+        if(error != nil) {
+            CASLog(@"Failed to load events from file, error: %@", error.localizedDescription);
+            queue = @[];
+        }
+    } else {
+        @try {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            queue = [NSKeyedUnarchiver unarchiveObjectWithFile:self.storagePath];
+#pragma clang diagnostic pop
+        } @catch (NSException *exception) {
+            CASLog(@"Failed to load events from file, error: %@", [exception callStackSymbols]);
+            queue = @[];
+        }
+    }
     CASLog(@"%ld events read from: %@", queue.count, self.storagePath);
     if (queue == nil) {
         queue = @[];
