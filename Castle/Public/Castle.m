348476340ec67397facb4db02c12e591143398af
Alexander Simson <alex@simsons.se>
2022-05-10 14:08:32 +0200

    Add check to determine if the SDK has been configured in resetConfiguration. (#94)

--- Castle/Public/Castle.m
+++ Castle/Public/Castle.m
@@ -124,6 +124,11 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 
 + (void)resetConfiguration
 {
+    // If the SDK hasn't been configured, no reset is necessary
+    if (![Castle isConfigured]) {
+        return;
+    }
+    
     Castle *castle = [Castle sharedInstance];
     CastleConfiguration *configuration = castle.configuration;
     
