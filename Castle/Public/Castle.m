031171fe00eaa535a5b709af0346ab80224a79c9
Alexander Simson <alex@simsons.se>
2025-10-02 14:46:01 +0200

    Include CastleConfiguration implementation in Castle.h/m to avoid header import issue in SwiftPM and bundled xcframework.

--- Castle/Public/Castle.m
+++ Castle/Public/Castle.m
@@ -27,6 +27,54 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+static NSString *CastleConfigurationDefaultAPIDomain = @"m.castle.io";
+static NSString *CastleConfigurationCastleAPIPath = @"v1/";
+
+@interface CastleConfiguration ()
+@property (nonatomic, copy, readwrite) NSString *publishableKey;
+@end
+
+@implementation CastleConfiguration
+
++ (instancetype _Nonnull)configurationWithPublishableKey:(NSString * _Nonnull)publishableKey
+{
+    NSAssert([publishableKey hasPrefix:@"pk_"], @"You must provide a valid Castle publishable key when initializing the SDK.");
+
+    CastleConfiguration *configuration = [[CastleConfiguration alloc] init];
+    configuration.publishableKey = publishableKey;
+    configuration.screenTrackingEnabled = NO;
+    configuration.debugLoggingEnabled = NO;
+    configuration.flushLimit = 20;
+    configuration.maxQueueLimit = 1000;
+    configuration.enableAdvertisingTracking = YES;
+    configuration.enableApplicationLifecycleTracking = YES;
+    return configuration;
+}
+
+#pragma mark - Setters
+
+- (void)setBaseURLAllowList:(nullable NSArray *)baseURLAllowList
+{
+    NSMutableArray *allowlist = @[].mutableCopy;
+    for (NSURL *url in baseURLAllowList) {
+        if([url isKindOfClass:NSURL.class]) {
+            // Only add the base URL discarding any other components of the provided URL
+            [allowlist addObject:[NSURL URLWithString:[NSString stringWithFormat:@"%@://%@/", url.scheme, url.host]]];
+        }
+    }
+    _baseURLAllowList = allowlist.copy;
+}
+
+#pragma mark - Getters
+
+- (NSURL *)baseURL
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"https://%@/%@", CastleConfigurationDefaultAPIDomain, CastleConfigurationCastleAPIPath]];
+}
+
+@end
+
+
 static Castle *_sharedClient = nil;
 
 NSString *const CastleUserJwtKey = @"CastleUserJwtKey";
